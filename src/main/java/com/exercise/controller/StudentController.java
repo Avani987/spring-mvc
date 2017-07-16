f982fd5bb40fc8b4d30603d074dbca50cdd67ea4
avani987 <avani.9khurana@gmail.com>
2017-07-16 21:42:15 +0530

    Q2, use /jsp in url for first part of question and /httpresponse for second part

--- src/main/java/com/exercise/controller/StudentController.java
+++ src/main/java/com/exercise/controller/StudentController.java
@@ -1,18 +1,30 @@
 package com.exercise.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+@Controller
+@RequestMapping("/")
+public class StudentController extends MultiActionController {
 
-public class StudentController extends AbstractController{
+    @RequestMapping("/jsp")
+    protected ModelAndView actionforJspView(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-    protected ModelAndView handleRequestInternal(HttpServletRequest request,HttpServletResponse response) throws Exception{
-
-        ModelAndView modelAndView=new ModelAndView("index");
+        ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("welcomeMessage","Hello from index.jsp");
         return modelAndView;
     }
 
+    @RequestMapping("/httpresponse")
+    protected void actionforHttpResponse(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        response.setContentType("text/html");
+        response.getWriter().print("Hello from HttpResponse Object");
+    }
 
 }
+
--- src/main/webapp/WEB-INF/views/jsp/index.jsp
+++ src/main/webapp/WEB-INF/views/jsp/index.jsp
@@ -2,7 +2,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <body>
-    <h1>Q1</h1>
+    <h1>Q2</h1>
     <h2>${welcomeMessage}</h2>
 </body>
 </html>

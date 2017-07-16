4afee8ef4a53b2b55035415a4cd2c662b5a52013
avani987 <avani.9khurana@gmail.com>
2017-07-16 21:48:17 +0530

    Use /hello in url to access the action hello that returns response Body

--- classes/artifacts/mvc_exercise_war_exploded/WEB-INF/views/jsp/index.jsp
+++ classes/artifacts/mvc_exercise_war_exploded/WEB-INF/views/jsp/index.jsp
@@ -2,7 +2,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <body>
-    <h1>Q1</h1>
+    <h1>Q4</h1>
     <h2>This is jsp page</h2>
 </body>
 </html>
--- src/main/java/com/exercise/controller/StudentController.java
+++ src/main/java/com/exercise/controller/StudentController.java
@@ -2,18 +2,27 @@ package com.exercise.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 @Controller
+@RequestMapping("/")
 public class StudentController {
 
-    @RequestMapping("/index.html")
+    @RequestMapping("/index")
     ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView("index");
         return modelAndView;
+    }
+    
+    @ResponseBody
+    @RequestMapping("/hello")
+    String Hello()
+    {
+        return "Hello world";
     }
 }
 
